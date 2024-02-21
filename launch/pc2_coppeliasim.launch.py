259de51658a6bf6dc2340cd6728e699dd353f65c
Your Name <you@example.com>
2024-02-21 09:12:29 +0100

    Minor

---
+++ launch/pc2_coppeliasim.launch.py
@@ -0,0 +1,48 @@
+# Import necessary Python modules
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Declare launch arguments
+        DeclareLaunchArgument('input_topic', default_value='/Float32MultiArray_in'),
+        DeclareLaunchArgument('output_topic', default_value='/cloud_out'),
+        DeclareLaunchArgument('frame_id', default_value='/camera'),
+        DeclareLaunchArgument('near_clip', default_value='0.01'),
+        DeclareLaunchArgument('far_clip', default_value='3.5'),
+        DeclareLaunchArgument('view_angle', default_value='57'),
+        DeclareLaunchArgument('height', default_value='480'),
+        DeclareLaunchArgument('width', default_value='640'),
+        DeclareLaunchArgument('open_rviz', default_value='true'),
+
+        # Define the float32multiarray_to_pointcloud2 node
+        Node(
+            package='pc2_coppeliasim',
+            executable='pc2_coppeliasim',
+            name='pc2_coppeliasim',
+            output='screen',
+            parameters=[
+                {'frame_id': LaunchConfiguration('frame_id')},
+                {'input_topic': LaunchConfiguration('input_topic')},
+                {'output_topic': LaunchConfiguration('output_topic')},
+                {'near_clip': LaunchConfiguration('near_clip')},
+                {'far_clip': LaunchConfiguration('far_clip')},
+                {'view_angle': LaunchConfiguration('view_angle')},
+                {'height': LaunchConfiguration('height')},
+                {'width': LaunchConfiguration('width')}
+            ],
+            respawn=True
+        ),
+
+        # Conditionally launch RViz based on the 'open_rviz' argument
+        #Node(
+        #    package='rviz2',
+        #    executable='rviz2',
+        #    name='rviz',
+        #    arguments=['-d', LaunchConfiguration('rviz_config')],
+        #    condition=IfCondition(LaunchConfiguration('open_rviz'))
+        #)
+    ])
---
+++ src/pc2_coppeliasim.cpp
@@ -0,0 +1,116 @@
+// ROS2
+#include "rclcpp/rclcpp.hpp"
+
+// String msg
+#include "std_msgs/msg/string.hpp"
+// Float32MultiArray
+#include "std_msgs/msg/float32_multi_array.hpp"
+// PointCloud2
+#include "sensor_msgs/msg/point_cloud2.hpp"
+
+// PCL
+#include "pcl/conversions.h"
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include "pcl/filters/passthrough.h"
+
+// Other
+#include <iostream>
+
+#define M_PI 3.14159265358979323846
+#define degreesToRadians(angleDegrees) (angleDegrees * M_PI / 180.0)
+
+class Float32MultiArrayToPointCloud2 : public rclcpp::Node
+{
+public:
+  Float32MultiArrayToPointCloud2()
+    : Node("float32multiarray_to_pointcloud2")
+  {
+    // Get parameters
+    this->declare_parameter<std::string>("input_topic", "Float32MultiArray_in");
+    this->declare_parameter<std::string>("output_topic", "cloud_out");
+    this->declare_parameter<std::string>("frame_id", "map");
+    this->declare_parameter<float>("near_clip", 0.02);
+    this->declare_parameter<float>("far_clip", 3.5);
+    this->declare_parameter<float>("view_angle", 57);
+    this->declare_parameter<int>("height", 480);
+    this->declare_parameter<int>("width", 640);
+
+    this->get_parameter("input_topic", input_topic_);
+    this->get_parameter("output_topic", output_topic_);
+    this->get_parameter("frame_id", frame_id_);
+    this->get_parameter("near_clip", near_clip_);
+    this->get_parameter("far_clip", far_clip_);
+    this->get_parameter("view_angle", view_angle_);
+    this->get_parameter("height", height_);
+    this->get_parameter("width", width_);
+
+    // Set up publisher
+    pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(output_topic_, 1);
+
+    // Set up subscriber
+    sub_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
+      input_topic_, 1000, std::bind(&Float32MultiArrayToPointCloud2::chatterCallback, this, std::placeholders::_1));
+    
+    scale_ = (far_clip_ - near_clip_) / 1.0;
+    // unsigned int datalen = height_ * width_;
+    float view_angle_r = degreesToRadians(view_angle_);
+    f_ = float(float(std::max(height_, width_)) / 2) / float(tan(view_angle_r / 2));
+
+  }
+
+private:
+  void chatterCallback(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
+  {
+    std::vector<float> depth_raw = msg->data;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    for (int j = 0; j < height_; j++)
+    {
+      float y = (j - height_ / 2.0);
+      for (int i = 0; i < width_; i++)
+      {
+        int k = j * width_ + i;
+        float x = -(i - width_ / 2.0);
+        x_scale.push_back(float(x / f_));
+        y_scale.push_back(float(y / f_));
+
+        float depth = near_clip_ + scale_ * depth_raw[k];
+        float xyz[3] = {depth * x_scale[k], depth * y_scale[k], depth};
+        pcl::PointXYZRGB p;
+
+        p.x = xyz[0];
+        p.y = xyz[1];
+        p.z = xyz[2];
+        p.r = 0;
+        p.g = 0;
+        p.b = 0;
+        cloud->points.push_back(p);
+      }
+    }
+  
+    sensor_msgs::msg::PointCloud2 output;
+    pcl::toROSMsg(*cloud.get(), output);
+    output.header.frame_id = frame_id_;
+    output.header.stamp = rclcpp::Clock().now();
+    pub_->publish(output);
+  }
+
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_;
+  rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_;
+
+  std::string input_topic_, output_topic_, frame_id_;
+  int height_, width_;
+  float near_clip_, far_clip_, view_angle_,scale_, f_;
+  std::vector<float> x_scale, y_scale;
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<Float32MultiArrayToPointCloud2>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
